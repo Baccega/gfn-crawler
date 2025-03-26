30e35df0c9092b159275a05add764acc4b33ea97
Sandro Baccega <mail.sandro.baccega@gmail.com>
2025-03-26 18:29:35 +0100

    ✨ Add failedUrls and processed index

--- osc/getDiscoutedPrice.ts
+++ osc/getDiscoutedPrice.ts
@@ -5,29 +5,34 @@ function splitText(text: string) {
 }
 
 export async function getDiscountedPrice(page: Page, url: string) {
-  await page.goto(url);
-  const codice = await page
-    .getByRole("heading", { name: "CODICE" })
-    .innerText();
-
-  const rows = await page
-    .locator(
-      '//*[@id="ctl00_cpholder_ctl00_salespricesctl_puom"]/table[2]/tbody/tr'
-    )
-    .all();
-
-  if (rows.length === 0) {
-    const rows2 = await page
-      .locator("#ctl00_cpholder_ctl00_pnlOutlet > span.bar.w.outlet")
+  try {
+    await page.goto(url);
+    const codice = await page
+      .getByRole("heading", { name: "CODICE" })
       .innerText();
-    return [codice.substring(7), ...rows2.split("\n")];
-  }
 
-  const firstRow = await rows[0].innerText();
-  const secondRow = rows[1] ? await rows[1].innerText() : "";
-  return [
-    codice.substring(7),
-    ...splitText(firstRow),
-    ...splitText(secondRow).filter(Boolean),
-  ];
+    const rows = await page
+      .locator(
+        '//*[@id="ctl00_cpholder_ctl00_salespricesctl_puom"]/table[2]/tbody/tr'
+      )
+      .all();
+
+    if (rows.length === 0) {
+      const rows2 = await page
+        .locator("#ctl00_cpholder_ctl00_pnlOutlet > span.bar.w.outlet")
+        .innerText();
+      return [codice.substring(7), ...rows2.split("\n")];
+    }
+
+    const firstRow = await rows[0].innerText();
+    const secondRow = rows[1] ? await rows[1].innerText() : "";
+    return [
+      codice.substring(7),
+      ...splitText(firstRow),
+      ...splitText(secondRow).filter(Boolean),
+    ];
+  } catch (error) {
+    console.log(`Error processing ${url}: ${error}`);
+    return;
+  }
 }
--- osc/main.ts
+++ osc/main.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+import "dotenv/config";
 import { chromium, devices } from "playwright";
 import { login, logout } from "./login.ts";
 import { getDiscountedPrice } from "./getDiscoutedPrice.ts";
@@ -6,31 +6,30 @@ import fs from "fs";
 import { readUrlsFromFile, wait } from "./utils.ts";
 
 const BATCH_SIZE = 100;
-const CSV_FILE = "discounted_prices.csv";
+const RESULTS_CSV_FILE = "discounted_prices.csv";
+const FAILED_FILE = "failedUrls.txt";
 // const FILE_PATH = "fullUrls.txt";
-const FILE_PATH = "filteredUrls.txt";
+// const FILE_PATH = "filteredUrls.txt";
+const FILE_PATH = "testUrls.txt";
 
 const writeToCsv = (data: string[]) => {
-  fs.appendFileSync(CSV_FILE, data.join("\n") + "\n");
+  fs.appendFileSync(RESULTS_CSV_FILE, data.join("\n") + "\n");
 };
 
 const processBatch = async (page, urls: string[]) => {
   const results: string[] = [];
 
-  for (const url of urls) {
-    try {
-      const discountedPrice = await getDiscountedPrice(page, url);
-      if (!discountedPrice) {
-        console.log(`No discounted price found for ${url}`);
-        continue;
-      }
-      const line = `${url};${discountedPrice[0]};${discountedPrice[1]};${discountedPrice[2]};${discountedPrice[3]};${discountedPrice[4]};${discountedPrice[5]};${discountedPrice[6]}`; 
-      results.push(line);
-      console.log(`Processed: ${url}`);
-      await wait(200);
-    } catch (error) {
-      console.log(`Error processing ${url}: ${error}`);
+  for (let i = 0; i < urls.length; i++) {
+    const url = urls[i];
+    const discountedPrice = await getDiscountedPrice(page, url);
+    if (!discountedPrice) {
+      fs.appendFileSync(FAILED_FILE, url + "\n");
+      continue;
     }
+    const line = `${url};${discountedPrice[0]};${discountedPrice[1]};${discountedPrice[2]};${discountedPrice[3]};${discountedPrice[4]};${discountedPrice[5]};${discountedPrice[6]}`;
+    results.push(line);
+    console.log(`Processed (${i + 1}/${urls.length}): ${url}`);
+    await wait(200);
   }
 
   // Write the results for this batch to the CSV file
@@ -48,9 +47,9 @@ const processBatch = async (page, urls: string[]) => {
   await login(page);
   console.log("Logged in");
 
-  if (!fs.existsSync(CSV_FILE)) {
+  if (!fs.existsSync(RESULTS_CSV_FILE)) {
     fs.writeFileSync(
-      CSV_FILE,
+      RESULTS_CSV_FILE,
       "URL;Codice;APartireDa;PrezzoUnitario;PrezzoPerPz;APartireDa2;PrezzoUnitario2;PrezzoPerPz2\n"
     );
   }
@@ -63,7 +62,7 @@ const processBatch = async (page, urls: string[]) => {
     console.log(`Batch ${Math.floor(i / BATCH_SIZE) + 1} completed.`);
 
     // Wait for user confirmation before processing the next batch (for manual verification)
-    const proceed = await new Promise((resolve) => {
+    await new Promise((resolve) => {
       process.stdout.write("Press Enter to continue to the next batch...");
       process.stdin.once("data", () => resolve(true));
     });
